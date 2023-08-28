69654f7cb91e9292380985aaee6e0d05c07ee919
Shahzad Akhtar Khan <shahzadakj1778@gmail.com>
2023-08-28 11:08:19 +0500

    commited a change

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   images: {
-    domains: ['res.cloudinary.com'], // Add your Cloudinary domain here
+    domains: ['dgmt0ksz6.cloudinary.com'], // Add your Cloudinary domain here
   },
   env: {
     CLOUDINARY_CLOUD_NAME: 'dgmt0ksz6',
